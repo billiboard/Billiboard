af84a8edf75c6ba31c555bf496b5c8f62635c375
erlking87 <erlking87@gmail.com>
2016-11-24 17:25:00 +0900

    modify

--- app/src/main/java/com/pool/taiho/billiboard/MainActivity.java
+++ app/src/main/java/com/pool/taiho/billiboard/MainActivity.java
@@ -1,13 +1,42 @@
 package com.pool.taiho.billiboard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import com.pool.taiho.billiboard.layout.ChooseTypeActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resInit();
+    }
+
+    public void resInit() {
+        videoView = (VideoView)findViewById(R.id.videoView);
+        final MediaController mediaController = new MediaController(this);
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.wildlife);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(video);
+        videoView.requestFocus();
+        videoView.setOnTouchListener(this);
+        videoView.start();
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        Intent intent = new Intent(getApplicationContext(), ChooseTypeActivity.class);
+        startActivity(intent);
+        return false;
     }
 }
---
+++ app/src/main/java/com/pool/taiho/billiboard/layout/ChooseTypeActivity.java
@@ -0,0 +1,18 @@
+package com.pool.taiho.billiboard.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.pool.taiho.billiboard.R;
+
+/**
+ * Created by Taiho on 2016-11-24.
+ */
+
+public class ChooseTypeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choosetype);
+    }
+}
